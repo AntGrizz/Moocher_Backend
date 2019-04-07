d311dbc13ad20784e3051d0543930985d357b785
Anthony Gregg <agregg22@gmail.com>
2019-04-07 00:03:42 -0400

    updated user schema

--- moocher/app/models/user.rb
+++ moocher/app/models/user.rb
@@ -9,11 +9,11 @@ class User < ApplicationRecord
   has_many :groups, through: :user_groups
 
   def user_serializer
-    {name: self.name, username: self.username, street: self.street, city: self.city, state: self.state, zip_code: self.zip_code, owner_rating: self.user_rating, renter_rating: self.renter_rating}
+    {first_name: self.first_name, last_name: self.last_name, username: self.username, street: self.street, city: self.city, state: self.state, zip_code: self.zip_code, owner_rating: self.user_rating, renter_rating: self.renter_rating}
   end
 
   def user_items_serializer
-    {name: self.name, username: self.username, street: self.street, city: self.city, state: self.state, zip_code: self.zip_code, owner_rating: self.user_rating, renter_rating: self.renter_rating, items: self.items}
+    {first_name: self.first_name, last_name: self.last_name, username: self.username, street: self.street, city: self.city, state: self.state, zip_code: self.zip_code, owner_rating: self.user_rating, renter_rating: self.renter_rating, items: self.items}
   end
 
 end
--- moocher/db/schema.rb
+++ moocher/db/schema.rb
@@ -55,7 +55,8 @@ ActiveRecord::Schema.define(version: 2019_04_02_185111) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.string "username"
     t.string "street"
     t.string "city"
