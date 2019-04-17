39dfb3628ad21cffd49dbdd90766b9f9117a44c2
Anthony Gregg <agregg22@gmail.com>
2019-04-17 00:40:36 -0400

    updated the create item method

--- moocher/app/controllers/items_controller.rb
+++ moocher/app/controllers/items_controller.rb
@@ -6,6 +6,16 @@ class ItemsController < ApplicationController
     render json: items.map{|item| item.item_serializer}
   end
 
+  def create
+    @item = Item.create!(item_params)
+    if @item.save
+    token = request.headers["Authentication"].split(' ')[1]
+    payload = decode(token)
+    @user = User.find(payload["user_id"])
+      render json: @user.user_items_serializer
+    end
+  end
+
   def destroy
     token = request.headers["Authentication"].split(' ')[1]
     payload = decode(token)
@@ -18,6 +28,10 @@ class ItemsController < ApplicationController
 
 
 private
+
+  def item_params
+    params.require(:item).permit(:name, :user_id, :description, :image, :condition)
+  end
 
   def get_item
     @item = Item.find(params[:id])
